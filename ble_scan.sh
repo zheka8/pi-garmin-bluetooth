e7e014e0e92e2c7ea07ff2a33d80aff0cbd2ea02
EK <a@a.com>
2021-04-29 17:23:01 +0100

    adds logic to reset interface in case of fault

--- ble_scan.sh
+++ ble_scan.sh
@@ -6,7 +6,7 @@
 # Scan params
 SCAN_INTERVAL=10      #time between scans (seconds)
 SCAN_DURATION="2"     #duration of each scan (seconds)
-SCAN_QUERY="RTL"      #target device
+SCAN_QUERY="RTL23101" #target device
 NUM_FOUND=0           #number of devices found that match target
 NUM_MISSES=0          #number of times target not found
 NUM_MISSES_TO_STOP=8  #number of times the target is not found before stopping rec
@@ -19,12 +19,12 @@ SEG_DURATION=3600000  # (ms)  new file every hour
 WIDTH=1280
 HEIGHT=720
 
-# Log params
-LOG_FILE="status.log"
+# Data
+ERR_FILE="errors.txt"
 
 # Path
-full_path=$(realpath $0)
-dir_path=$(dirname $full_path)
+FULL_PATH=$(realpath $0)
+DIR_PATH=$(dirname $FULL_PATH)
 
 update_state () {
     # check if recording is on
@@ -42,7 +42,7 @@ start_video_recording () {
     echo "Starting video recording"
     FILE_NAME=`date +"%Y_%m_%d_%H_%M"`
     
-    raspivid -o "$dir_path/data/$FILE_NAME.h264" \
+    raspivid -o "$DIR_PATH/data/$FILE_NAME.h264" \
 	     -t 0 -s -sg $SEG_DURATION \
 	     -w $WIDTH -h $HEIGHT &
     
@@ -55,11 +55,27 @@ stop_video_recording () {
     update_state
 }
 
+toggle_interface () {
+    # in case of I/O errors, set bluetooth interface down and up
+    sudo hciconfig hci0 down
+    sudo hciconfig hci0 up
+}
+
+check_for_errors () {
+    # check if there were errors from previous scan and try to reset
+    NUM_ERRORS=$(wc -l "$DIR_PATH/data/$ERR_FILE" | awk {'print $1;'})
+
+    if [[ $NUM_ERRORS -gt 0 ]]
+    then
+        toggle_interface
+    fi 
+}
+
 scan () {
     NUM_FOUND=$(timeout -s SIGINT "$SCAN_DURATION"s \
-    	        hcitool -i hci0 lescan | grep "$SCAN_QUERY" | wc -l)
-
-    # keep track consecutive absences of target device
+    	        hcitool -i hci0 lescan 2>"$DIR_PATH/data/$ERR_FILE" \
+		| grep "$SCAN_QUERY" | wc -l)
+    
     if [[ $NUM_FOUND -eq 0 ]]
     then
         let NUM_MISSES=$NUM_MISSES+1
@@ -72,8 +88,6 @@ scan () {
 }
 
 main_loop () {
-    echo "Loop here"
-    
     while [ 1 ]
     do
 	# perform the scan and verify if recording is currently on
@@ -86,17 +100,10 @@ main_loop () {
         elif [[ $NUM_MISSES -ge $NUM_MISSES_TO_STOP ]] && [[ $IS_RECORDING -ge 0 ]]
         then
 	    stop_video_recording
-        else
-	    echo "WHY ARE WE HERE, $NUM_MISSES, $IS_RECORDING"
 	fi
-
 
 	sleep $SCAN_INTERVAL
     done
 }
 
 main_loop
-
-#start_video_recording
-#sleep 5
-#stop_video_recording
