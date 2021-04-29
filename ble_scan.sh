48c7b198cfa49ff3d220df5ee73b22a9e1d62339
EK <a@a.com>
2021-04-29 11:13:05 +0100

    check recording status and other improvements

--- ble_scan.sh
+++ ble_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Periodically scan for bluetooth low energy devices and
-# perform an action if the desired device is found
+# enable video recording if the device is found
 
 # Scan params
 SCAN_INTERVAL=2       #time between scans (seconds)
@@ -9,23 +9,47 @@ SCAN_DURATION="2"     #duration of each scan (seconds)
 SCAN_QUERY="RTL"      #target device
 NUM_FOUND=0           #number of devices found that match target
 NUM_MISSES=0          #number of times target not found
-NUM_MISSES_TO_STOP=8  #number of times the target is not found before action is taken
+NUM_MISSES_TO_STOP=8  #number of times the target is not found before stopping rec
 
-# Recording state (# find raspivid proc instead?)
-IS_RECORDING=0
+# Recording state
+IS_RECORDING=-1
+
+# Recording params
+SEG_DURATION=3600000  # (ms)  new file every hour
+WIDTH=1024
+HEIGHT=768
+
+update_state () {
+    # check if recording is on
+    IS_RECORDING=$(pgrep raspivid)
+
+    if [[ -z "$IS_RECORDING" ]]
+    then
+        IS_RECORDING=-1
+    fi
+
+    echo "IS_RECORDING $IS_RECORDING"
+}
 
 start_video_recording () {
     echo "Starting video recording"
-    IS_RECORDING=1
+    FILE_NAME=`date +"%Y_%m_%d_%H_%M"`
+    
+    raspivid -o data/$FILE_NAME.h264 -t 0 -s -sg $SEG_DURATION \
+	     -w $WIDTH -h $HEIGHT &
+    
+    update_state
 }
 
 stop_video_recording () {
     echo "Stopping video recording"
-    IS_RECORDING=0
+    pkill raspivid
+    update_state
 }
 
 scan () {
-    NUM_FOUND=$(sudo timeout -s SIGINT "$SCAN_DURATION"s hcitool -i hci0 lescan | grep "$SCAN_QUERY" | wc -l)
+    NUM_FOUND=$(sudo timeout -s SIGINT "$SCAN_DURATION"s \
+	        hcitool -i hci0 lescan | grep "$SCAN_QUERY" | wc -l)
 
     # keep track consecutive absences of target device
     if [[ $NUM_FOUND -eq 0 ]]
@@ -37,7 +61,6 @@ scan () {
 
     echo "NUM FOUND: $NUM_FOUND"
     echo "NUM MISSED: $NUM_MISSES"
-
 }
 
 main_loop () {
@@ -45,12 +68,14 @@ main_loop () {
     
     while [ 1 ]
     do
+	# perform the scan and verify if recording is currently on
         scan
+	update_state
 
-        if [[ $NUM_FOUND -gt 0 ]] && [[ $IS_RECORDING -eq 0 ]] 
+        if [[ $NUM_FOUND -gt 0 ]] && [[ $IS_RECORDING -eq -1 ]] 
         then
 	    start_video_recording
-        elif [[ $NUM_MISSES -ge $NUM_MISSES_TO_STOP ]] && [[ $IS_RECORDING -eq 1 ]]
+        elif [[ $NUM_MISSES -ge $NUM_MISSES_TO_STOP ]] && [[ $IS_RECORDING -ge 0 ]]
         then
 	    stop_video_recording
         fi
@@ -60,3 +85,7 @@ main_loop () {
 }
 
 main_loop
+
+#start_video_recording
+#sleep 5
+#stop_video_recording
